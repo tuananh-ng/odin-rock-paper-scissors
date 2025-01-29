dabe7617b787abab4cc32559f3fe430a89df1114
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-01-29 07:22:45 +0100

    Add a function to play the game with a number of rounds
    Make the function used to play a round to be a helper function of this.

--- .js
+++ .js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let numChoice = Math.floor(Math.random() * 3);
     switch (numChoice) {
@@ -28,38 +25,60 @@ function getHumanChoice() {
     return choice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    /* get the choices of the human and the computer
-    Based on the rule deciding which choice is superior, increase the score of the player
-    Print a congratulatory message
-    */
-    if (!humanChoice) {
-        computerScore += 1;
-        console.log("You missed your turn!");
-        return;
-    }
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
+    let numRound = 5;
 
-    if (humanChoice === computerChoice) {
+    for (let i = 0; i < numRound; i++) {
+        console.log(`Round ${i + 1}:`);
+        computerSelection = getComputerChoice();
+        humanSelection = getHumanChoice();
+        console.log(`Computer: ${computerSelection}\nYou: ${humanSelection}`);
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    console.log("Overall:");
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    } else if (computerScore > humanScore) {
+        console.log("You lose!");
+    } else {
         console.log("It's a tie!");
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        computerScore += 1;
-        console.log("You lose! Paper beats Rock");
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        humanScore += 1;
-        console.log("You win! Rock beats Scissors");
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        humanScore += 1;
-        console.log("You win! Paper beats Rock");
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        computerScore += 1;
-        console.log("You lose! Scissors beat Paper");
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        computerScore += 1;
-        console.log("You lose! Rock beats Scissors");
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore += 1;
-        console.log("You win! Scissors beat paper");
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        if (!humanChoice) {
+            computerScore += 1;
+            console.log("You missed your turn!");
+            return;
+        }
+        humanChoice = humanChoice.toLowerCase();
+        computerChoice = computerChoice.toLowerCase();
+    
+        if (humanChoice == computerChoice) {
+            console.log("It's a tie!");
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            computerScore += 1;
+            console.log("You lose! Paper beats Rock");
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            humanScore += 1;
+            console.log("You win! Rock beats Scissors");
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            humanScore += 1;
+            console.log("You win! Paper beats Rock");
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            computerScore += 1;
+            console.log("You lose! Scissors beat Paper");
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            computerScore += 1;
+            console.log("You lose! Rock beats Scissors");
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore += 1;
+            console.log("You win! Scissors beat paper");
+        }
     }
 }
