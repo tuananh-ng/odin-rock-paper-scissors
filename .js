f0c73b3722cdd1d87989b7f74f39bd03923df100
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-01-29 05:49:20 +0100

    Add a function to randomly generate computer's choice

---
+++ .js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    numChoice = Math.floor(Math.random() * 3);
+    switch (numChoice) {
+        case 0:
+            console.log("rock");
+            break;
+        case 1:
+            console.log("paper");
+            break;
+        case 2:
+            console.log("scissors");
+            break;
+    }
+}
