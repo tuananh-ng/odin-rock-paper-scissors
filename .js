510da7b5e7ff4d22f456224c0f61774df0067198
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-01-29 07:26:10 +0100

    Update the function used to play a round
    Replace the tie test with a strict equality test to be more robust.

--- .js
+++ .js
@@ -59,7 +59,7 @@ function playGame() {
         humanChoice = humanChoice.toLowerCase();
         computerChoice = computerChoice.toLowerCase();
     
-        if (humanChoice == computerChoice) {
+        if (humanChoice === computerChoice) {
             console.log("It's a tie!");
         } else if (humanChoice === "rock" && computerChoice === "paper") {
             computerScore += 1;
