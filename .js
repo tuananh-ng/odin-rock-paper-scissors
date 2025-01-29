479d55a68cb2e09fb0d6398956cca5bbf2e37d3e
tuananh-ng <158744840+tuananh-ng@users.noreply.github.com>
2025-01-29 06:48:41 +0100

    Add a function to play a round

--- .js
+++ .js
@@ -27,3 +27,43 @@ function getHumanChoice() {
     }
     console.log(choice);
 }
+
+function playRound(humanChoice, computerChoice) {
+    /* get the choices of the human and the computer
+    Based on the rule deciding which choice is superior, increase the score of the player
+    Print a congratulatory message
+    */
+    if (!humanChoice) {
+        return;
+    }
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        humanScore -= 1;
+        computerScore += 1;
+        console.log("You lose! Paper beats Rock");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore += 1;
+        computerScore -= 1;
+        console.log("You win! Rock beats Scissors");
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore += 1;
+        computerScore -= 1;
+        console.log("You win! Paper beats Rock");
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        humanScore -= 1;
+        computerScore += 1;
+        console.log("You lose! Scissors beat Paper");
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        humanScore -= 1;
+        computerScore += 1;
+        console.log("You lose! Rock beats Scissors");
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore += 1;
+        computerScore -= 1;
+        console.log("You win! Scissors beat paper");
+    }
+}
